8c5435eeb422963842f8b5f9970ec6eb2426d1e1
naruepan-th <naruepan.th@gmail.com>
2024-10-08 13:17:25 -0500

    finished implementation of stack

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # algogroup-question2-stack
+to compile run: 
+$ g++ -o option2 option2.cpp
+to run compiled file: 
+$ ./option2
+
--- option2.cpp
+++ option2.cpp
@@ -4,6 +4,7 @@
 #include <cstdint>
 #include <cstdlib>
 #include <vector>
+#include <iostream>
 
 using namespace std;
 
@@ -15,6 +16,75 @@ using namespace std;
 // `peek` returns the value at the top
 // `size` returns the count of values
 
-class IntStack {
-    
+struct intlist {
+    int integer;
+    intlist* next;
+
+    intlist(int i, intlist* ptr) : integer(i), next(ptr) {}
 };
+
+intlist* prepend(int integer, intlist* next) {
+    intlist* newIntlist = new intlist(integer, next);
+    return newIntlist;
+}
+
+class IntStack {
+private:
+    intlist* head;
+    int sizeOfStack;
+public:
+    IntStack(int integer) : sizeOfStack(1){
+        head = new intlist(integer, nullptr);
+    }
+
+    void push(int integer) {
+        intlist* newNode = new intlist(integer, head);
+        head = newNode;
+        sizeOfStack++;
+    }
+
+    int pop() {
+        if (head == nullptr) {
+            std::cout << "stack is empty" <<std::endl;
+            return 0;
+        }
+        int res = head->integer;
+        head = head->next;
+        sizeOfStack--;
+        return res;
+    }
+
+    int peek() {
+        if (head == nullptr) {
+            std::cout << "stack is empty" <<std::endl;
+            return 0;
+        }
+        return head->integer;
+    }
+
+    int size() {
+        return sizeOfStack;
+    }
+
+    void printStack() {
+        intlist* tmp = head;
+        while (tmp) {
+            std::cout << tmp->integer << std::endl;
+            tmp = tmp->next;
+        }
+    }
+};
+
+int main() {
+    IntStack stack = IntStack(10);
+    stack.push(20);
+    stack.push(30);
+    std::cout << stack.size() << std::endl;
+    stack.printStack();
+    int topOfStack = stack.peek();
+    std::cout << topOfStack <<std::endl;
+
+    stack.pop();
+    stack.printStack();
+    std::cout << stack.size() << std::endl;
+}
